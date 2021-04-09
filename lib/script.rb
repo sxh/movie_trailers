7123cf15d35c8477d5fcf415fdb5ae19a2a45a8b
sxh <steve.e.hayes@gmail.com>
2021-04-09 18:53:33 +0100

    Ready to start with real data

--- lib/script.rb
+++ lib/script.rb
@@ -1,23 +1,20 @@
+# frozen_string_literal: true
+
 require 'mechanize'
 require 'json'
 
-class MovieDb
-
-  def initialize(mechanize_page)
-    @json = JSON.parse(mechanize_page.body)
+# utility class for anything retrieved from The MovieDB api
+class MovieDbApi
+  def initialize(api_base, query_parameters = '')
+    page = api_page_from(api_base, query_parameters)
+    @json = JSON.parse(page.body)
   end
 
   def results
     @json['results']
   end
 
-  def agent
-    @agent ||= Mechanize.new
-  end
-
-  def get(url)
-    agent.get(url)
-  end
+  private
 
   def base_url
     'https://api.themoviedb.org/3'
@@ -33,33 +30,48 @@ class MovieDb
   end
 
   def api_page_from(api_base, query_parameters = '')
-    agent.get(api_url_from(api_base, query_parameters))
+    Mechanize.new.get(api_url_from(api_base, query_parameters))
   end
-
 end
 
 # result of calling the videos api for a given movie
-class MovieVideos < MovieDb
-
+class MovieVideos < MovieDbApi
   def initialize(movie_id)
-    page= api_page_from("/movie/#{movie_id}/videos")
-    super(page)
-  end
-
-  def youtube_ids
-    results
-      .select{|each| each['type'].eql?('Trailer') && each['site'].eql?('YouTube') }
-      .collect{|each| each['key']}
+    super("/movie/#{movie_id}/videos")
   end
 
   def trailers
-    youtube_ids.collect{ |each| TrailerVideo.new(each) }
+    trailer_youtube_ids.collect { |each| TrailerVideo.new(each) }
   end
 
+  private
+
+  def trailer_youtube_ids
+    results
+      .select { |each| each['type'].eql?('Trailer') && each['site'].eql?('YouTube') }
+      .collect { |each| each['key'] }
+  end
 end
 
-class Movie
+# result of calling the a search
+class Search < MovieDbApi
+  def initialize(name, year)
+    super('/search/movie', "&query=#{CGI.escape(name)}&year=#{year}")
+  end
 
+  def movies
+    movie_ids.collect { |each| Movie.new(each) }
+  end
+
+  private
+
+  def movie_ids
+    results.collect { |each| each['id'] }
+  end
+end
+
+# Single movie, source of all related videos
+class Movie
   def initialize(movie_id)
     @movie_id = movie_id
   end
@@ -67,31 +79,10 @@ class Movie
   def videos
     MovieVideos.new(@movie_id)
   end
-
 end
 
-# result of calling the a search
-class Search < MovieDb
-
-  def initialize(name, year)
-    page = api_page_from("/search/movie", "&query=#{CGI.escape(name)}&year=#{year}")
-    super(page)
-  end
-
-  def movies
-    movie_ids.collect{ |each| Movie.new(each) }
-  end
-
-  private
-
-  def movie_ids
-    results.collect{ |each| each['id'] }
-  end
-
-end
-
+# Single video, that we believe is a trailer
 class TrailerVideo
-
   def initialize(youtube_video_id)
     @vid = youtube_video_id
   end
@@ -99,11 +90,12 @@ class TrailerVideo
   def download
     system("youtube-dl -o '~/Movies/%(title)s-trailer.%(ext)s' #{@vid} --restrict-filenames", exception: true)
   end
-
 end
 
-name = 'Jack Reacher'
-year = 2012
-Search.new(name, year).movies.each do |movie|
-  movie.videos.trailers.each(&:download)
+[
+  ['Jack Reacher', 2012]
+].each do |name, year|
+  Search.new(name, year).movies.each do |movie|
+    movie.videos.trailers.each(&:download)
+  end
 end
