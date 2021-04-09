25420d87660407f2ae8e988a5c38a23805520454
sxh <steve.e.hayes@gmail.com>
2021-04-09 17:58:14 +0100

    Started to hack a search together

--- lib/script.rb
+++ lib/script.rb
@@ -3,27 +3,50 @@ require 'json'
 
 api_key = '9c762f8e2cb0c83962e7c51008e43906'
 
-# result of calling the videos api for a given movie
-class Videos
+class MovieDb
+
   def initialize(mechanize_page)
     @json = JSON.parse(mechanize_page.body)
   end
 
-  def youtube_ids
+  def results
     @json['results']
+  end
+
+end
+
+# result of calling the videos api for a given movie
+class Videos < MovieDb
+
+  # def initialize(mechanize_page)
+  #   @json = JSON.parse(mechanize_page.body)
+  # end
+
+  def youtube_ids
+    results
       .select{|each| each['type'].eql?('Trailer') && each['site'].eql?('YouTube') }
       .collect{|each| each['key']}
   end
 
 end
 
-agent = Mechanize.new
-page = agent.get("https://api.themoviedb.org/3/movie/550/videos?api_key=#{api_key}")
-trailer_ids = Videos.new(page).youtube_ids
-trailer_ids.each do |vid|
-  # retrieve and put in Movies
-  system("youtube-dl -o '~/Movies/%(title)s-trailer.%(ext)s' #{vid} --restrict-filenames", exception: true)
+# result of calling the a search
+class Search < MovieDb
+
 end
+
+agent = Mechanize.new
+page = agent.get("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=Jack+Reacher")
+search = Search.new(page)
+pp search.results
+# https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+movie_id = '550'
+page = agent.get("https://api.themoviedb.org/3/movie/#{movie_id}/videos?api_key=#{api_key}")
+trailer_ids = Videos.new(page).youtube_ids
+# trailer_ids.each do |vid|
+#   # retrieve and put in Movies
+#   system("youtube-dl -o '~/Movies/%(title)s-trailer.%(ext)s' #{vid} --restrict-filenames", exception: true)
+# end
 pp trailer_ids
 
 # youtube-dl -o '%(title)s-trailer.%(ext)s' BdJKm16Co6M --restrict-filenames
@@ -35,6 +58,9 @@ pp trailer_ids
 # preview = youtube.download_preview
 # => "tmp/downloads/zzG4K2m_j5U.jpg"
 
+
+# https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+#
 # https://api.themoviedb.org/3/movie/550?api_key=9c762f8e2cb0c83962e7c51008e43906
 #
 # https://api.themoviedb.org/3/movie/550/videos?api_key=9c762f8e2cb0c83962e7c51008e43906
