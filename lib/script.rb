61a6dda53180bffb5cf58d035599437831a4657f
sxh <steve.e.hayes@gmail.com>
2021-04-09 18:33:32 +0100

    Merging into classes

--- lib/script.rb
+++ lib/script.rb
@@ -13,19 +13,32 @@ class MovieDb
     @json['results']
   end
 
+  def agent
+    @agent ||= Mechanize.new
+  end
+
+  def get(url)
+    agent.get(url)
+  end
+
 end
 
 # result of calling the videos api for a given movie
 class MovieVideos < MovieDb
 
-  # def initialize(mechanize_page)
-  #   @json = JSON.parse(mechanize_page.body)
-  # end
+  def initialize(movie_id)
+    api_key = '9c762f8e2cb0c83962e7c51008e43906'
+    super(agent.get("https://api.themoviedb.org/3/movie/#{movie_id}/videos?api_key=#{api_key}"))
+  end
 
   def youtube_ids
     results
       .select{|each| each['type'].eql?('Trailer') && each['site'].eql?('YouTube') }
       .collect{|each| each['key']}
+  end
+
+  def trailers
+    youtube_ids.collect{ |each| TrailerVideo.new(each) }
   end
 
 end
@@ -37,10 +50,7 @@ class Movie
   end
 
   def videos
-    api_key = '9c762f8e2cb0c83962e7c51008e43906'
-    agent = Mechanize.new
-    page = agent.get("https://api.themoviedb.org/3/movie/#{@movie_id}/videos?api_key=#{api_key}")
-    MovieVideos.new(page)
+    MovieVideos.new(@movie_id)
   end
 
 end
@@ -48,26 +58,26 @@ end
 # result of calling the a search
 class Search < MovieDb
 
-  def initialize()
+  def initialize(name, year)
     api_key = '9c762f8e2cb0c83962e7c51008e43906'
     agent = Mechanize.new
-    name = 'Jack Reacher'
-    year = 2012
     page = agent.get("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{CGI.escape(name)}&year=#{year}")
     super(page)
-  end
-
-  def movie_ids
-    results.collect{ |each| each['id'] }
   end
 
   def movies
     movie_ids.collect{ |each| Movie.new(each) }
   end
 
+  private
+
+  def movie_ids
+    results.collect{ |each| each['id'] }
+  end
+
 end
 
-class Trailer
+class TrailerVideo
 
   def initialize(youtube_video_id)
     @vid = youtube_video_id
@@ -79,14 +89,8 @@ class Trailer
 
 end
 
-search = Search.new()
-pp search.movie_ids
-
-search.movies.each do |movie|
-  # page = agent.get("https://api.themoviedb.org/3/movie/#{movie_id}/videos?api_key=#{api_key}")
-  trailer_ids = movie.videos.youtube_ids
-  trailer_ids.each do |vid|
-    Trailer.new(vid).download
-  end
-  pp trailer_ids
+name = 'Jack Reacher'
+year = 2012
+Search.new(name, year).movies.each do |movie|
+  movie.videos.trailers.each(&:download)
 end
