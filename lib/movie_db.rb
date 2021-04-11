c25e8772e9e34d3deee0a3de5a44337f0554652c
sxh <steve.e.hayes@gmail.com>
2021-04-11 18:17:59 +0100

    Module

--- lib/movie_db.rb
+++ lib/movie_db.rb
@@ -3,96 +3,95 @@
 require 'mechanize'
 require 'json'
 
-# utility class for anything retrieved from The MovieDB api
-class MovieDbApi
-  def initialize(api_base, query_parameters = '')
-    page = api_page_from(api_base, query_parameters)
-    @json = JSON.parse(page.body)
+module TheMovieDb
+  # utility class for anything retrieved from The MovieDB api
+  class MovieDbApi
+    def initialize(api_base, query_parameters = '')
+      page = api_page_from(api_base, query_parameters)
+      @json = JSON.parse(page.body)
+    end
+
+    def results
+      @json['results']
+    end
+
+    private
+
+    def base_url
+      'https://api.themoviedb.org/3'
+    end
+
+    def auth
+      api_key = '9c762f8e2cb0c83962e7c51008e43906'
+      "?api_key=#{api_key}"
+    end
+
+    def api_url_from(api_base, query_parameters = '')
+      base_url + api_base + auth + query_parameters
+    end
+
+    def api_page_from(api_base, query_parameters = '')
+      Mechanize.new.get(api_url_from(api_base, query_parameters))
+    end
   end
 
-  def results
-    @json['results']
+  # result of calling the videos api for a given movie
+  class MovieVideos < MovieDbApi
+    def initialize(movie_id)
+      super("/movie/#{movie_id}/videos")
+    end
+
+    def trailers
+      trailer_youtube_ids.collect { |each| TrailerVideo.new(each) }
+    end
+
+    private
+
+    def trailer_youtube_ids
+      results
+        .select { |each| each['type'].eql?('Trailer') && each['site'].eql?('YouTube') }
+        .collect { |each| each['key'] }
+    end
   end
 
-  private
+  # result of calling the a search
+  class Search < MovieDbApi
+    def initialize(name, year)
+      super('/search/movie', "&query=#{CGI.escape(name)}&year=#{year}")
+    end
 
-  def base_url
-    'https://api.themoviedb.org/3'
+    def movies
+      movie_ids.collect { |each| Movie.new(each) }
+    end
+
+    private
+
+    def movie_ids
+      results.collect { |each| each['id'] }
+    end
   end
 
-  def auth
-    api_key = '9c762f8e2cb0c83962e7c51008e43906'
-    "?api_key=#{api_key}"
+  # Single movie, source of all related videos
+  class Movie
+    def initialize(movie_id)
+      @movie_id = movie_id
+    end
+
+    def videos
+      MovieVideos.new(@movie_id)
+    end
   end
 
-  def api_url_from(api_base, query_parameters = '')
-    base_url + api_base + auth + query_parameters
-  end
+  # Single video, that we believe is a trailer
+  class TrailerVideo
+    def initialize(youtube_video_id)
+      @vid = youtube_video_id
+    end
 
-  def api_page_from(api_base, query_parameters = '')
-    Mechanize.new.get(api_url_from(api_base, query_parameters))
-  end
-end
-
-# result of calling the videos api for a given movie
-class MovieVideos < MovieDbApi
-  def initialize(movie_id)
-    super("/movie/#{movie_id}/videos")
-  end
-
-  def trailers
-    trailer_youtube_ids.collect { |each| TrailerVideo.new(each) }
-  end
-
-  private
-
-  def trailer_youtube_ids
-    results
-      .select { |each| each['type'].eql?('Trailer') && each['site'].eql?('YouTube') }
-      .collect { |each| each['key'] }
-  end
-end
-
-# result of calling the a search
-class Search < MovieDbApi
-  def initialize(name, year)
-    super('/search/movie', "&query=#{CGI.escape(name)}&year=#{year}")
-  end
-
-  def movies
-    movie_ids.collect { |each| Movie.new(each) }
-  end
-
-  private
-
-  def movie_ids
-    results.collect { |each| each['id'] }
-  end
-end
-
-# Single movie, source of all related videos
-class Movie
-  def initialize(movie_id)
-    @movie_id = movie_id
-  end
-
-  def videos
-    MovieVideos.new(@movie_id)
-  end
-end
-
-# Single video, that we believe is a trailer
-class TrailerVideo
-  def initialize(youtube_video_id)
-    @vid = youtube_video_id
-  end
-
-  def download
-    begin
+    def download
       system("youtube-dl -o '~/Movies/%(title)s-trailer.%(ext)s' #{@vid} --restrict-filenames", exception: true)
     rescue Exception => e
       puts e.to_s
     end
   end
 end
-
--- lib/script.rb
+++ lib/script.rb
@@ -7,28 +7,8 @@ MovieLibrary.new.movie_directories.reject(&:has_trailer?).each do |dir|
   pp dir.has_trailer?
   pp dir.name.year
   pp dir.name.movie
-  Search.new(dir.name.movie, dir.name.year).movies.each do |movie|
+  TheMovieDb::Search.new(dir.name.movie, dir.name.year).movies.each do |movie|
     movie.videos.trailers.each(&:download)
   end
 
 end
-
-
-# [
-#   ['A History of Violence', 2005],
-#   ['City of God', 2002],
-#   ['E.T. the Extra-Terrestrial', 1982],
-#   ['End of Sentence', 2019],
-#   ['Far from Heaven', 2002],
-#   ['Lantana', 2001],
-#   ['Praise', 1998],
-#   ['Sex, Lies, and Videotape', 1989],
-#   ['The Long Kiss Goodnight', 1996],
-#   ['The Whistlers', 2019],
-#   ['While We Were Young', 2014],
-#   ['Working Man', 2020]
-# ].each do |name, year|
-#   Search.new(name, year).movies.each do |movie|
-#     movie.videos.trailers.each(&:download)
-#   end
-# end
