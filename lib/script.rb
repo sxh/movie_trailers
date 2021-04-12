4b9ee6596bc2c312752fff094b1452069c19a336
sxh <steve.e.hayes@gmail.com>
2021-04-12 10:39:22 +0100

    Changed label

--- lib/script.rb
+++ lib/script.rb
@@ -6,7 +6,7 @@ require_relative 'movie_nas'
 # change code copy videos to target directory
 MovieLibrary.new("/Volumes/Media/Movies").movie_directories.reject(&:has_trailer?).each do |movie_directory|
   puts '*************************************************************************'
-  puts "No trailers found for #{movie_directory.name.movie} #{movie_directory.name.year}"
+  puts "Haven't sourced trailers found for #{movie_directory.name.movie} #{movie_directory.name.year}"
   puts '*************************************************************************'
   TheMovieDb::Search.new(movie_directory.name.movie, movie_directory.name.year).movies.each do |movie|
     movie.videos.trailers.each{|trailer| trailer.download_to(movie_directory.path)}
