32f2ace194a52aa957b2437a31a2fd4fda077c33
sxh <steve.e.hayes@gmail.com>
2021-04-13 16:55:05 +0100

    More movie parsing

--- lib/movie_directory_name.rb
+++ lib/movie_directory_name.rb
@@ -2,18 +2,46 @@
 
 require 'forwardable'
 
+class Regexp
+
+  def +(other)
+    Regexp.new(self.to_s + other.to_s)
+  end
+
+end
+
+# Common regular expressions for movie years
+module YearRegexps
+  def year_regexp
+    /(19|20)\d{2}/
+  end
+
+  def year_in_parens_regexp
+    /\(/ + year_regexp + /\)/
+  end
+end
+
 # Wrapper around a particular strategy
 class MovieDirectoryName
   extend Forwardable
   def_delegators :@delegate, :year, :movie
 
   def initialize(name)
-    @delegate = MovieDirectoryNameWithParenthesisedYear.new(name)
+    @delegate = ParensYearName.applies_to?(name) ? ParensYearName.new(name) : DottedName.new(name)
   end
+
 end
 
 # Something like 'Spontaneous (2020)'
-class MovieDirectoryNameWithParenthesisedYear
+class ParensYearName
+
+  include YearRegexps
+  extend YearRegexps
+
+  def self.applies_to?(a_string)
+    !!a_string[year_in_parens_regexp]
+  end
+
   def initialize(name)
     @name = name
   end
@@ -29,10 +57,34 @@ class MovieDirectoryNameWithParenthesisedYear
   private
 
   def year_substring
-    @name[/\(\d+\)/]
+    @name[year_in_parens_regexp]
   end
 
   def year_substring_no_parens
     year_substring[1..-2]
   end
+end
+
+class DottedName
+
+  include YearRegexps
+
+  def initialize(name)
+    @name = name.gsub(".", " ")
+  end
+
+  def year
+    @name[year_index..(year_index+3)].to_i
+  end
+
+  def movie
+    @name[0..(year_index-1)].strip
+  end
+
+  private
+
+  def year_index
+    @name.index(year_regexp)
+  end
+
 end
--- spec/movie_directory_name_spec.rb
+++ spec/movie_directory_name_spec.rb
@@ -2,13 +2,19 @@ require 'movie_directory_name'
 
 describe MovieDirectoryName do
 
-  it 'should parse simple names' do
+  it 'should parse names like Spontaneous (2020)' do
     name = MovieDirectoryName.new('Spontaneous (2020)')
     expect(name.movie).to eql('Spontaneous')
     expect(name.year).to eql(2020)
   end
 
-  # The.Shape.of.Water.2017.1080p.BluRay.x264-SPARKS[hotpena]
+  it 'should parse names like The.Shape.of.Water.2017.1080p.BluRay.x264-SPARKS[hotpena]' do
+    name = MovieDirectoryName.new('The.Shape.of.Water.2017.1080p.BluRay.x264-SPARKS[hotpena]')
+    expect(name.movie).to eql('The Shape of Water')
+    expect(name.year).to eql(2017)
+  end
+
+  #
 
 
 end
